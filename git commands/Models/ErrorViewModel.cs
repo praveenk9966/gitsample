27c8cfd2c3ff62fc721e7ba984b4c2a4be6d441f
praveenk9966 <praveenkumarkoneti1994@gmail.com>
2021-07-31 13:39:47 +0530

    dot net files to repo

---
+++ git commands/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace git_commands.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
